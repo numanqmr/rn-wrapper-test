97c0d9d05e8758c1b78d6ffa08c4a16cadf7df26
jeremyfox <myfoxjere@gmail.com>
2022-05-07 14:11:43 +0500

    adding sdk via jitpack dependency and making adjustments to the the react sdk module

---
+++ android/src/main/java/com/landmarksid/rn/Const.java
@@ -0,0 +1,14 @@
+package com.landmarksid.rn;
+
+public class Const {
+    //Event names
+    public static String MESSAGE = "message";
+    public static String SUCCESS = "success";
+    public static String ERROR = "error";
+    public static String ON_LORE = "on_lore";
+    public static String ON_INIT = "on_init";
+    public static String ON_CONFIG = "on_config";
+
+    //SDK NAME
+    public static String RN_LANDMARKSID_SDK = "RNLandmarksidSDK";
+}
--- android/src/main/java/com/landmarksid/rn/RNLandmarksidSdkModule.java
+++ android/src/main/java/com/landmarksid/rn/RNLandmarksidSdkModule.java
@@ -1,97 +1,218 @@
 
 package com.landmarksid.rn;
 
-import android.app.Activity;
 import android.util.Log;
 
-import com.facebook.react.BuildConfig;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.landmarksid.lo.LandmarksID;
-import com.landmarksid.lo.formats.CustomData;
-import com.landmarksid.lo.listeners.EventListener;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.landmarksid.lo.core.LandmarksID;
+import com.landmarksid.lo.logging.EventLogListener;
+import com.landmarksid.lo.types.CustomData;
+import com.landmarksid.rn.BuildConfig;
 
+import org.jetbrains.annotations.NotNull;
 
-public class RNLandmarksidSdkModule extends ReactContextBaseJavaModule implements EventListener {
+import static com.landmarksid.rn.Const.ERROR;
+import static com.landmarksid.rn.Const.MESSAGE;
+import static com.landmarksid.rn.Const.ON_CONFIG;
+import static com.landmarksid.rn.Const.ON_INIT;
+import static com.landmarksid.rn.Const.ON_LORE;
+import static com.landmarksid.rn.Const.RN_LANDMARKSID_SDK;
+import static com.landmarksid.rn.Const.SUCCESS;
 
-    private final ReactApplicationContext reactContext;
+public class RNLandmarksidSdkModule extends ReactContextBaseJavaModule implements LifecycleEventListener, EventLogListener {
 
     private static final String TAG = RNLandmarksidSdkModule.class.getSimpleName();
-    private static final String API_KEY = "e4f1d1a4-5513-4ee1-941b-d80236ecae5c";
 
-    private static final String APP_ID = "5afd32d956f16b0004daebfc";
-    private static final String APP_SECRET = "oOPEhGYECnvYSEFmeHIsA2VPSwP3YGr9LEAMDhk5iGOOuHCx";
+
+    private final ReactApplicationContext reactContext;
+    private static LandmarksID landmarksId;
+
+    //TODO : secure the keys in .properties file for safety.
+    private static final String API_KEY = "755a32cf-00fd-4b85-8787-ce177b93d8a0";
+    private static final String APP_ID = "5df70cc3919e370004a297c2";
+    private static final String APP_SECRET = "97pxpfkzFi1Bn9F5TeIrotJsiq3KVPS3AzLNxPPWLwHy3ihO";
 
     private static final boolean IS_DEBUG_MODE = BuildConfig.DEBUG;
 
-    private LandmarksID landmarksId;
-    private Activity mActivity;
-
+    private CustomData customData = new CustomData();
 
     public RNLandmarksidSdkModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        this.mActivity = getCurrentActivity();
     }
 
     @ReactMethod
     public void startSDK() {
-        CustomData customData = new CustomData();
-        customData.addString("country", "de");
-        customData.addInt("countryCode", 49);
-        customData.addFloat("score", 23.58f);
-        customData.addBoolean("uralic", false);
 
         LandmarksID.Options options = new LandmarksID.Options()
                 .setApiKey(API_KEY)
                 .setAppMetadata(APP_ID, APP_SECRET)
                 .setDebugMode(IS_DEBUG_MODE)
-                .setCustomerId("sample-id")
-                .setCustomData(customData);
+                .setCustomerId("sample-id")// TODO add actual customer Id here.
+                .setCustomData(getDefaultData()); // TODO remove this when you are using your custom data
 
-        landmarksId = LandmarksID.getInstance().start(mActivity, options, this);
-        Log.d(TAG, "start SDK success: ");
-        try {
-            landmarksId.setLocationUsable(mActivity);
-        } catch (Exception e) {
-            Log.e(TAG, "start SDK failure: " + e.getLocalizedMessage());
-        }
+        landmarksId = LandmarksID.getInstance().start(getCurrentActivity(), options, this);
+    }
+
+    @ReactMethod
+    public void stopSDK(){
+        if (landmarksId != null)
+            landmarksId.stop(getCurrentActivity());
+    }
+
+    @ReactMethod
+    public void setCustomData(String name, String value) {
+        customData.add(name, value);
+        landmarksId.setCustomData(customData);
+    }
+
+    @ReactMethod
+    public void setCustomData(String name, Float value) {
+        customData.add(name, value);
+        landmarksId.setCustomData(customData);
+    }
+
+    @ReactMethod
+    public void setCustomData(String name, Integer value) {
+        customData.add(name, value);
+        landmarksId.setCustomData(customData);
+    }
+
+    @ReactMethod
+    public void setCustomData(String name, Boolean value) {
+        customData.add(name, value);
+        landmarksId.setCustomData(customData);
+    }
+
+    // Required for rn built in EventEmitter Calls else gives warning in console
+    @ReactMethod
+    public void addListener(String eventName) {
+
+    }
+    // Required for rn built in EventEmitter Calls else gives warning in console
+    @ReactMethod
+    public void removeListeners(Integer count) {
+
+    }
+
+    private CustomData getDefaultData(){
+        CustomData customData = new CustomData();
+        customData.add("country", "de");
+        customData.add("countryCode", 49);
+        customData.add("score", 23.58f);
+        customData.add("uralic", false);
+        return customData;
     }
 
     @Override
-    public String getName() {
-        return "RNLandmarksidSdk";
+    public @NotNull String getName() {
+        return RN_LANDMARKSID_SDK;
     }
 
+    @Override
+    public void onHostResume() {
+
+    }
+
+    @Override
+    public void onHostPause() {
+
+    }
+
+    @Override
+    public void onHostDestroy() {
+
+    }
+
+    private void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable WritableMap params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    /**
+     * Here we'll setup native events that can be subscribed in react-native code for android
+     * to receive live events.
+     */
     @Override
     public void msg(String tag, String message) {
-        //todo handle messages here
+        Log.i(TAG, "msg: " + "tag:" + tag + "message:" + message);
+
+        WritableMap params = Arguments.createMap();
+        params.putString("tag", tag);
+        params.putString("message", message);
+
+        sendEvent(reactContext, MESSAGE, params);
     }
 
     @Override
     public void success(String tag, String message) {
-        //todo handle success here
+        Log.i(TAG, "success: " + "tag:" + tag + "message:" + message);
+        WritableMap params = Arguments.createMap();
+        params.putString("tag", tag);
+        params.putString("message", message);
+
+        sendEvent(reactContext, SUCCESS, params);
+
     }
 
     @Override
     public void error(String tag, String message) {
-        //todo handle errors here
+        Log.e(TAG, "error: " + "tag:" + tag + "message:" + message);
+        WritableMap params = Arguments.createMap();
+        params.putString("tag", tag);
+        params.putString("message", message);
+
+        sendEvent(reactContext, ERROR, params);
+
 
     }
 
     @Override
     public void onLore(String tag, String time, String message, String type) {
-        //todo handle lore updates here
+        Log.i(TAG, "onLore: " + "tag:" + tag + "message:" + message);
+        WritableMap params = Arguments.createMap();
+        params.putString("tag", tag);
+        params.putString("time", time);
+        params.putString("message", message);
+        params.putString("type", type);
+
+        sendEvent(reactContext, ON_LORE, params);
+
     }
 
     @Override
     public void onInit(String tag, String message) {
-        //todo handle init updates here
+        Log.i(TAG, "onInIt: " + "tag:" + tag + "message:" + message);
+        WritableMap params = Arguments.createMap();
+        params.putString("tag", tag);
+        params.putString("message", message);
+
+        sendEvent(reactContext, ON_INIT, params);
+
     }
 
     @Override
-    public void onConfig(boolean androidEnabled, long timeInterval, long distanceInterval, long minSpeedKph, long maxSpeedKph) {
-        //todo handle config updates here
+    public void onConfig(final boolean androidEnabled, final long timeInterval, final long distanceInterval, final long minSpeedKph, final long maxSpeedKph) {
+        WritableMap params = Arguments.createMap();
+        params.putBoolean("androidEnabled", androidEnabled);
+        params.putDouble("timeInterval", timeInterval);
+        params.putDouble("distanceInterval", distanceInterval);
+        params.putDouble("minSpeedKph", minSpeedKph);
+        params.putDouble("maxSpeedKph", maxSpeedKph);
+
+        sendEvent(reactContext, ON_CONFIG, params);
+
     }
 }
